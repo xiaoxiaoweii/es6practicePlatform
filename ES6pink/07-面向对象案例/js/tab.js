7c1577d1de66326b94d8f7e19e0fea81cc1816ec
xiaoxiaoweii <2679656468@qq.com>
2020-08-24 23:30:29 +0800

    tab切换功能实现

--- ES6pink/07-面向对象案例/js/tab.js
+++ ES6pink/07-面向对象案例/js/tab.js
@@ -1,6 +1,8 @@
+var that;
 class Tab {
 	constructor(id) {
 		/* 获取元素 */
+		that = this;
 		this.main = document.querySelector(id);
 		this.lis = this.main.querySelectorAll('li');
 		this.sections = this.main.querySelectorAll('section');
@@ -10,13 +12,21 @@ class Tab {
 		/* init 初始化操作让相关的元素绑定事件 */
 		for (var i = 0; i < this.lis.length; i++) {
 			this.lis[i].index = i;
-			this.lis[i].onclick = function () {
-				console.log(this.index);
-			};
+			this.lis[i].onclick = this.toggleTab;
 		}
 	}
 	/* 1.切换功能 */
-	toggleTab() {}
+	toggleTab() {
+		that.clearClass();
+		this.className = 'liactive';
+		that.sections[this.index].className = 'conactive';
+	}
+	clearClass() {
+		for (var i = 0; i < this.lis.length; i++) {
+			this.lis[i].className = '';
+			this.sections[i].className = '';
+		}
+	}
 	/* 2.添加功能 */
 	addTab() {}
 	/* 3.删除功能 */
